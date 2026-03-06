1424e39367ca05ca62e14f8b7577c31d52d08800
niculinpaul <niculinpaul@protonmail.com>
2026-03-06 11:45:46 +0200

    Create nicu.py

---
+++ nicu.py
@@ -0,0 +1,41 @@
+from seleniumbase import SB
+import random
+import base64
+import requests
+geo_data = requests.get("http://ip-api.com/json/").json()
+proxy_str = False
+urlt = f"https://www.twitch.tv/brutalles"
+while True:
+    with SB(uc=True, locale="en",ad_block=True,chromium_arg='--disable-webgl',proxy = proxy_str) as attww:
+        rnd = random.randint(450,800)
+        
+        attww.activate_cdp_mode(urlt,tzone=f"{timezone_id}",geoloc=(latitude, longitude))
+        attww.sleep(2)
+        if attww.is_element_present('button:contains("Accept")'):
+            attww.cdp.click('button:contains("Accept")', timeout=4)
+        attww.sleep(2)    
+        attww.sleep(12)
+        if attww.is_element_present('button:contains("Start Watching")'):
+            attww.cdp.click('button:contains("Start Watching")', timeout=4)
+            attww.sleep(10)
+        if attww.is_element_present('button:contains("Accept")'):
+            attww.cdp.click('button:contains("Accept")', timeout=4)
+        if attww.is_element_present("#live-channel-stream-information"):
+        
+            if attww.is_element_present('button:contains("Accept")'):
+                attww.cdp.click('button:contains("Accept")', timeout=4)
+            if True:
+                attww2 = attww.get_new_driver(undetectable=True)
+                attww2.activate_cdp_mode(urlt,tzone=f"{timezone_id}",geoloc=(latitude, longitude))
+                attww2.sleep(10)
+                if attww2.is_element_present('button:contains("Start Watching")'):
+                    attww2.cdp.click('button:contains("Start Watching")', timeout=4)
+                    attww2.sleep(10)
+                if attww2.is_element_present('button:contains("Accept")'):
+                    attww2.cdp.click('button:contains("Accept")', timeout=4)
+                attww.sleep(10)
+                attww.sleep(rnd)
+
+        else:
+            break
+
